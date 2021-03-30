cda0b9550f80f75a31f65c5e0e81605ff9f3a510
Saba Rahman <sabarahman.official@gmail.com>
2021-03-30 19:12:34 +0600

    Finished without styling.

--- todo/views.py
+++ todo/views.py
@@ -6,6 +6,7 @@ from django.contrib.auth import login, logout, authenticate
 from . forms import TodoForm
 from . models import Todo
 from django.utils import timezone
+from django.contrib.auth.decorators import login_required
 
 
 def home(request):
@@ -49,11 +50,13 @@ def logoutuser(request):
         return redirect('home')
 
 
+@login_required
 def currenttodos(request):
     todos = Todo.objects.filter(user=request.user, completed__isnull=True)
     return render(request, 'todo/currenttodos.html', {'todos': todos})
 
 
+@login_required
 def createtodos(request):
     if request.method == "GET":
         return render(request, 'todo/createtodos.html', {'form': TodoForm()})
@@ -65,6 +68,7 @@ def createtodos(request):
         return redirect('currenttodos')
 
 
+@login_required
 def viewtodo(request, todo_pk):
     todo = get_object_or_404(Todo, pk=todo_pk, user=request.user)
     if request.method == "GET":
@@ -76,6 +80,7 @@ def viewtodo(request, todo_pk):
         return redirect('currenttodos')
 
 
+@login_required
 def completetodo(request, todo_pk):
     todo = get_object_or_404(Todo, pk=todo_pk, user=request.user)
     if request.method == "POST":
@@ -84,6 +89,7 @@ def completetodo(request, todo_pk):
         return redirect('currenttodos')
 
 
+@login_required
 def deletetodo(request, todo_pk):
     todo = get_object_or_404(Todo, pk=todo_pk, user=request.user)
     if request.method == "POST":
@@ -91,6 +97,7 @@ def deletetodo(request, todo_pk):
         return redirect('currenttodos')
 
 
+@login_required
 def completedtodos(request):
     todos = Todo.objects.filter(
         user=request.user, completed__isnull=False).order_by('-completed')
